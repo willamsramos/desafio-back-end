c2a645b73140319af02e9f63d37fe7aa5a760763
willams ramos <willamsramos82@hotmail.com>
2024-10-11 06:07:55 -0300

    projeto-finalizado

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('node:path')
+const router = require('./routes')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(router)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => console.log(`Servidor iniciado em http://localhost:${PORT}`))
+
+
